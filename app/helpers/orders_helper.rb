2b00ef58e4c419468f33bfc7407322af8c53ff02
Ge Hao <althoughghgh@gmail.com>
2015-03-30 21:08:28 +0800

    on page:change time_down有问题，多次ajax

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,7 +4,32 @@ module OrdersHelper
     when 0
       "尽快送达"
     else
-      dead_line.to_i
+      del = dead_line.to_i - Time.now.to_i
+      
+      s = del % 60
+      m = (del / 60) % 60
+      h = del / 3600
+
+      if h == 0
+        h = ""
+      elsif h > 0 && h < 10
+        h = "0#{h}小时"
+      else
+        h = "#{h}小时"
+      end
+
+      if m < 10
+        m = "0#{m}分"
+      else
+        m = "#{m}分"
+      end
+
+      if s < 10
+        s = "0#{s}秒"
+      else
+        s = "#{s}秒"
+      end
+      "还剩：#{h}#{m}#{s}"
     end
   end
 
