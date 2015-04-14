5c5c931e5f375fe98874709a5a84cd5804aec311
Ge Hao <althoughghgh@gmail.com>
2015-04-14 19:57:28 +0800

    changed to with devise

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, :except => [:index]
+  before_action :authenticate_user!, :except => [:index, :show, :search]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
-
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find_by_name(params[:name])
     @orders = @user.orders.all
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find_by_name(params[:name])
-    end
 
 end
